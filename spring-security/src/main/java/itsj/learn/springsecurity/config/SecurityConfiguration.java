5ebb089aa8a8938f8fc57272a3d2d50d19d73a84
LahiruViet <48759351+LahiruViet@users.noreply.github.com>
2019-04-12 17:48:37 +0700

    added source

---
+++ spring-security/src/main/java/itsj/learn/springsecurity/config/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package itsj.learn.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                    .anyRequest()
+                    //.permitAll()
+                    .fullyAuthenticated()
+                    .and()
+                    .httpBasic();
+
+        httpSecurity.csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("abc").password(encoder.encode("abc")).roles("USER");
+
+    }
+}
